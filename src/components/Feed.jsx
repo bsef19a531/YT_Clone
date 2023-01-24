ba34455fcb4617bc7d5f145cfbc853ff75728409
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2023-01-24 23:58:09 +0500

    Implemented Video and Channel Cards

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -9,31 +9,30 @@ const Feed = () => {
     const [selectedCategory, setSelectedCategory] = useState('New');
     const [videos, setVideos] = useState([]);
     useEffect(() => {
-        setVideos(null);
+        // setVideos(null);
 
         fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-            .then((data) => setVideos(data.items)).catch(err => { console.log(err) })
+            .then((data) => setVideos(data.items))
     }, [selectedCategory]);
 
     return (
         <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-
-            <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+            <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
                 <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
 
-                <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: "#fff" }}>
-                    Copyright 2023 @M.Abdullah
+                <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+                    Copyright © 2023 @M.Abdullah
                 </Typography>
             </Box>
 
-            <Box p={2} sx={{ overflowY: 'auto', heigth: '90vh' }} >
-                <Typography variant='h5' fontWeight='bold' mb={2} sx={{ color: 'white', }}>
-                    Watch {selectedCategory + " "}<span style={{ color: '#fc1503' }}> Videos</span>
+            <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+                <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+                    Watch  {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
                 </Typography>
-            </Box>
 
-            <Videos videos={videos} />
-        </Stack >
+                <Videos videos={videos} />
+            </Box>
+        </Stack>
     )
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Box } from '@mui/material';
+import VideoCard from './VideoCard';
+import ChannelCard from './ChannelCard';
 
-const Videos = ({videos}) => {
-    console.log(videos);
+const Videos = ({ videos }) => {
     return (
-        <div>Videos</div>
+        <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+            {videos.map((item, idx) => (
+                <Box key={idx}>
+                    {item.id.videoId && <VideoCard video={item} />}
+                    {item.id.channelId && <ChannelCard channelDetail={item} />}
+                </Box>
+            ))}
+        </Stack>
     )
 }
 
