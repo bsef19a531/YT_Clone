3500bcae0f1df08633ef742ee07ce08e0ead5b1e
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2023-01-28 13:08:45 +0500

    Made Channel Page Gradient Dynamic

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -6,14 +6,38 @@ import { Videos, ChannelCard } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+
     const [channelDetail, setChannelDetail] = useState();
     const [videos, setVideos] = useState(null);
 
+    const gradientSettingsObj = {
+        gradientDegree: getRandomNumber(0, 360),
+        percentageColor1: getRandomNumber(0, 10),
+        percentageColor2: getRandomNumber(30, 60),
+        percentageColor3: getRandomNumber(90, 100),
+        rbg1: {
+            r: getRandomNumber(0, 255),
+            b: getRandomNumber(0, 255),
+            g: getRandomNumber(0, 255),
+        },
+        rbg2: {
+            r: getRandomNumber(0, 255),
+            b: getRandomNumber(0, 255),
+            g: getRandomNumber(0, 255),
+        },
+        rbg3: {
+            r: getRandomNumber(0, 255),
+            b: getRandomNumber(0, 255),
+            g: getRandomNumber(0, 255),
+        }
+    }
 
-    const [gradientDegree, setGradientDegree] = useState(getRandomNumber(0, 360));
-    const [percentageColor1, setPercentageColor1] = useState(getRandomNumber(0, 10));
-    const [percentageColor2, setPercentageColor2] = useState(getRandomNumber(30, 70));
-    const [percentageColor3, setPercentageColor3] = useState(getRandomNumber(90, 100));
+    const [gradientSettings, setGradientSettings] = useState(gradientSettingsObj);
+
+    // const [gradientDegree, setGradientDegree] = useState(getRandomNumber(0, 360));
+    // const [percentageColor1, setPercentageColor1] = useState(getRandomNumber(0, 10));
+    // const [percentageColor2, setPercentageColor2] = useState(getRandomNumber(30, 70));
+    // const [percentageColor3, setPercentageColor3] = useState(getRandomNumber(90, 100));
 
     const { id } = useParams();
 
@@ -42,7 +66,7 @@ const ChannelDetail = () => {
             <Box>
                 <div style={{
                     height: '300px',
-                    background: `linear-gradient(${gradientDegree}deg, rgba(0,238,247,1) ${percentageColor1}%, rgba(206,3,184,1) ${percentageColor2}%, rgba(0,212,255,1) ${percentageColor3}%)`,
+                    background: `linear-gradient(${gradientSettings.gradientDegree}deg, rgba(${gradientSettings.rbg1.r},${gradientSettings.rbg1.b},${gradientSettings.rbg1.g},1) ${gradientSettings.percentageColor1}%, rgba(${gradientSettings.rbg2.r},${gradientSettings.rbg2.b},${gradientSettings.rbg2.g},1) ${gradientSettings.percentageColor2}%, rgba(${gradientSettings.rbg3.r},${gradientSettings.rbg3.b},${gradientSettings.rbg3.g},1) ${gradientSettings.percentageColor3}%)`,
                     zIndex: 10,
                 }} />
                 <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
